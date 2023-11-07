6a9f0a61628f02022272d49b9103b8b63257b31d
Filip Hasoň <hasonfilip@seznam.cz>
2023-11-07 18:41:04 +0100

    Just some random stuff or idk, cba

---
+++ second/morerandomstuff.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <title>Form</title>
+</head>
+
+<?php
+$name = filter_input(INPUT_GET, "name") ?? "";
+$age = filter_input(INPUT_GET, "age", FILTER_VALIDATE_INT) ?? "";
+$luck = filter_input(INPUT_GET, "luck", FILTER_VALIDATE_INT) ?? "";
+$intelligence = filter_input(INPUT_GET, "intelligence", FILTER_VALIDATE_INT) ?? "";
+
+?>
+
+<form action="./submit.php" method="get">
+    <label for="name">Name</label>
+    <input name="name" value="<?= $name ?>">
+    <label for="age">Age</label>
+    <input name="age" min="0" max="100" value="<?= $age ?>">
+    <label for="luck">Luck</label>
+    <input name="luck" min="0" max="100" value="<?= $luck ?>">
+    <label for="intelligence">Intelligence</label>
+    <input name="intelligence" min="0" max="100" value="<?= $intelligence ?>">
+    <button type="submit">Submit</button>
+</form>
+
+<p>Well it basically does absolutely nothing - just like me D:</p>
+
+</html>
